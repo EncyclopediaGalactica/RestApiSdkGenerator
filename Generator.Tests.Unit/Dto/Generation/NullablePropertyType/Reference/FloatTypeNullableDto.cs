63dfd4fdf16247b9f4ae142265bb508def0aa12f
Andras Csanyi <andras.csanyi@pm.me>
2023-01-06 08:43:39 +0100

    test

---
+++ Generator.Tests.Unit/Dto/Generation/NullablePropertyType/Reference/FloatTypeNullableDto.cs
@@ -0,0 +1,17 @@
+namespace EncyclopediaGalactica.RestApiSdkGenerator.Generator.Tests.Unit.Dto.Generation.NullablePropertyType;
+
+/// <summary>
+///     The FloatTypeNullableDto object.
+/// </summary>
+public class FloatTypeNullableDto
+{
+    /// <summary>
+    ///     Gets or set the FloatTypeNullable property's value
+    /// </summary>
+    public float? FloatTypeNullable { get; set; }
+
+    /// <summary>
+    ///     Gets or set the FloatType property's value
+    /// </summary>
+    public float FloatType { get; set; }
+}
---
+++ Generator.Tests.Unit/Dto/Generation/NullablePropertyType/Reference/StringNullableDto.cs
@@ -0,0 +1,17 @@
+namespace EncyclopediaGalactica.RestApiSdkGenerator.Generator.Tests.Unit.Dto.Generation.NullablePropertyType;
+
+/// <summary>
+///     The StringNullableDto object.
+/// </summary>
+public class StringNullableDto
+{
+    /// <summary>
+    ///     Gets or set the StringTypeNullable property's value
+    /// </summary>
+    public string? StringTypeNullable { get; set; }
+
+    /// <summary>
+    ///     Gets or set the StringType property's value
+    /// </summary>
+    public string StringType { get; set; }
+}
