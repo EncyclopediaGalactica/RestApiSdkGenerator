f78150fd2aa7c39e9602cc6438e110e8a28b85d7
András Csányi <andras.csanyi@pm.me>
2023-01-06 08:48:09 +0100

    chore: nullable type generation tests
    This PR adds tests covering the nullable type generation in Dto phase

---
+++ Generator.Tests.Unit/Dto/Generation/NullablePropertyType/Reference/IntegerTypeNullableDto.cs
@@ -0,0 +1,17 @@
+namespace EncyclopediaGalactica.RestApiSdkGenerator.Generator.Tests.Unit.Dto.Generation.NullablePropertyType;
+
+/// <summary>
+///     The IntegerTypeNullableDto object.
+/// </summary>
+public class IntegerTypeNullableDto
+{
+    /// <summary>
+    ///     Gets or set the IntegerTypeNullable property's value
+    /// </summary>
+    public int? IntegerTypeNullable { get; set; }
+
+    /// <summary>
+    ///     Gets or set the IntegerType property's value
+    /// </summary>
+    public int IntegerType { get; set; }
+}
