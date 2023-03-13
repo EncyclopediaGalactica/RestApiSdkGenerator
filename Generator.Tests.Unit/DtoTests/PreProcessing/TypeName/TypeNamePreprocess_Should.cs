b09277b735b027067f88d75ff4372b9ac01d22fc
Andras Csanyi <andras.csanyi@pm.me>
2023-03-13 20:47:07 +0100

    fix

--- Generator.Tests.Unit/DtoTests/PreProcessing/TypeName/TypeNamePreprocess_Should.cs
+++ Generator.Tests.Unit/DtoTests/PreProcessing/TypeName/TypeNamePreprocess_Should.cs
@@ -30,7 +30,7 @@ public class TypeNamePreprocess_Should : TestBase
     public void PreProcess_MultipleFilenames()
     {
         // Arrange && Act
-        string currentPath = $"{_basePath}/DtoTests/PreProcessing/Typename";
+        string currentPath = $"{_basePath}/DtoTests/PreProcessing/TypeName";
         string configFilePath = $"{currentPath}/multiple_filename.json";
         CodeGenerator? codeGenerator = null;
         Action action = () => { codeGenerator = new CodeGenerator.Builder().SetPath(configFilePath).Generate(); };
